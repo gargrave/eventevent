29f9760c34eb32b47182f08d41742b40c777dacb
Gabe Hargrave <gkhargrave@gmail.com>
2018-04-29 14:41:45 -0700

    Throw together a quick CSS Modules example

--- src/modules/common/HomePage/HomePage.jsx
+++ src/modules/common/HomePage/HomePage.jsx
@@ -1,9 +1,13 @@
 // @flow
 import React from 'react';
 
+import styles from './HomePage.css';
+
 const HomePage = () => (
   <div>
     <h2>HomePage</h2>
+    <p className={styles.infoText}>This is info text!</p>
+    <p className={styles.dangerText}>This is danger text!</p>
   </div>
 );
 
