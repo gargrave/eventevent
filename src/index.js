f472c4c69754e04a581196ca664ca4b55926ccc9
Gabe Hargrave <gkhargrave@gmail.com>
2018-04-29 19:09:10 -0700

    Install Semantic UI

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
 import AppWrapper from './modules/core/AppWrapper';
+
+import './index.css';
 
 import registerServiceWorker from './registerServiceWorker';
 
--- src/modules/common/components/HomePage/HomePage.jsx
+++ src/modules/common/components/HomePage/HomePage.jsx
@@ -6,8 +6,6 @@ import styles from './HomePage.css';
 const HomePage = () => (
   <div>
     <h2>HomePage</h2>
-    <p className={styles.infoText}>This is info text!</p>
-    <p className={styles.dangerText}>This is danger text!</p>
   </div>
 );
 
