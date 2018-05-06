2be3173d0afe49ea4f701cdc16203e233459d05b
Gabe Hargrave <gkhargrave@gmail.com>
2018-05-06 10:20:35 -0700

    Rename LoginPageContainer

--- src/modules/auth/routes.js
+++ src/modules/auth/routes.js
@@ -1,11 +1,11 @@
 // @flow
 import type { ReactRoute } from '../core/flowtypes';
 
-import ConnectedLoginPage from './containers/ConnectedLoginPage/ConnectedLoginPage';
+import LoginPageContainer from './containers/LoginPageContainer/LoginPageContainer';
 
 const routes: ReactRoute[] = [
   {
-    component: ConnectedLoginPage,
+    component: LoginPageContainer,
     exact: true,
     path: '/account/login',
   },
